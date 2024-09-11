dce63a0e135b81f02a9678a4d034dcfbd0d4347c
RubénCano21 <ruben217.rc@gmail.com>
2024-09-10 23:05:11 -0400

    segundo commit

--- src/main/java/uagrm/bo/workflow/WorkflowApplication.java
+++ src/main/java/uagrm/bo/workflow/WorkflowApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WorkflowApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WorkflowApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WorkflowApplication.class, args);
+    }
 
 }
