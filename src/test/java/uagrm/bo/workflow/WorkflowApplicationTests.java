e1a37e53ed11ef11e662eb7b73c821edeb674a87
RubénCano21 <ruben217.rc@gmail.com>
2024-09-10 22:54:09 -0400

    primer commit

---
+++ src/test/java/uagrm/bo/workflow/WorkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package uagrm.bo.workflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
